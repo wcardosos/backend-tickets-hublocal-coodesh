156954be9c84f66c935184c87b6e7fd195e0d7ae
Wagner Cardoso <wagnerdev01@gmail.com>
2022-05-21 11:45:59 -0300

    feat(app/controller) Add login

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,8 +1,24 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { Response } from 'express';
 
 @Controller()
 export class AppController {
+  @UseGuards(AuthGuard('local'))
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  async login(@Res() response) {
+    return response.json({ message: 'Logged' });
+  }
+
   @Get('health-check')
   healthCheck(@Res() response: Response): Response {
     return response.json({ message: 'The system is working' });
