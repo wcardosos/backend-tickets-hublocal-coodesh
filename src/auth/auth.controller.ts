777029f38abfc2d12d41514c70d01e71596ac123
Wagner Cardoso <wagnerdev01@gmail.com>
2022-05-21 23:28:57 -0300

    feat(auth) Add login endpoint

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,27 @@
+import {
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './local-auth.guard';
+
+@Controller()
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async login(@Req() request, @Res() response) {
+    const { user } = request;
+
+    const token = this.authService.login(user);
+
+    return response.json({ token });
+  }
+}
