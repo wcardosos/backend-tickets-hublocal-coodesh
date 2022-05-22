42dff061f8bc3b5f8a22998caae3d01064d0e62f
Wagner Cardoso <wagnerdev01@gmail.com>
2022-05-21 23:24:54 -0300

    feat(app/controller) Remove login

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,24 +1,8 @@
-import {
-  Controller,
-  Get,
-  HttpCode,
-  HttpStatus,
-  Post,
-  Res,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
-import { LocalAuthGuard } from '../auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  @UseGuards(LocalAuthGuard)
-  @HttpCode(HttpStatus.OK)
-  @Post('login')
-  async login(@Res() response) {
-    return response.json({ message: 'Logged' });
-  }
-
   @Get('health-check')
   healthCheck(@Res() response: Response): Response {
     return response.json({ message: 'The system is working' });
