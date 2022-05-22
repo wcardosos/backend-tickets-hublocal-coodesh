f870fabb0718059753c31077137cf5f1fbec7b9c
Wagner Cardoso <wagnerdev01@gmail.com>
2022-05-22 13:41:15 -0300

    refactor(schema) Remove enterprise's responsible id

---
+++ prisma/migrations/20220522163514_remove_unnecessary_id_link_enterprise_model/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `responsibleId` on the `Enterprise` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Enterprise" DROP COLUMN "responsibleId";
