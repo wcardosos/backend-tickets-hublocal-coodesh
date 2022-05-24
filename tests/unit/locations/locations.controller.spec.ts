3cac6899c3eb913d993187db72418ae7850e299b
Wagner Cardoso <wagnerdev01@gmail.com>
2022-05-24 19:46:23 -0300

    feat(locations) Add find by id endpoint

--- tests/unit/locations/locations.controller.spec.ts
+++ tests/unit/locations/locations.controller.spec.ts
@@ -1,4 +1,6 @@
+import { HttpStatus } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { response, Response } from 'express';
 import { LocationsController } from '../../../src/locations/locations.controller';
 import { LocationsService } from '../../../src/locations/locations.service';
 
@@ -6,9 +8,19 @@ describe('LocationsController', () => {
   let locationsController: LocationsController;
   let locationsService: LocationsService;
 
+  const findByIdMock = jest.fn();
   const locationsServiceMock = {
     create: jest.fn(),
+    findById: findByIdMock,
   };
+
+  const responseMock = {} as Response;
+  const responseJsonMock = jest.fn();
+  responseMock.json = responseJsonMock;
+  responseMock.status = jest.fn().mockReturnValue({
+    json: responseJsonMock,
+  });
+  responseMock.end = jest.fn();
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -46,6 +58,28 @@ describe('LocationsController', () => {
       expect(locationsService.create).toHaveBeenCalledWith(
         createLocationDataMock,
       );
+    });
+  });
+
+  describe('findById', () => {
+    it('Should return a location', async () => {
+      findByIdMock.mockResolvedValueOnce('location');
+
+      const result = await locationsController.findById('id', responseMock);
+
+      expect(locationsService.findById).toHaveBeenCalledWith('id');
+      expect(result).toBe('location');
+    });
+
+    it('Should return a error message when the location not exists', async () => {
+      findByIdMock.mockResolvedValueOnce(null);
+
+      await locationsController.findById('id', responseMock);
+
+      expect(responseMock.status).toHaveBeenCalledWith(HttpStatus.NOT_FOUND);
+      expect(responseJsonMock).toHaveBeenCalledWith({
+        message: 'Location not found',
+      });
     });
   });
 });
