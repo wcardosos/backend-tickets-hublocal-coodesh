a6c956746b0648b4c2ad4c2061eb0b421210a6ba
Wagner Cardoso <wagnerdev01@gmail.com>
2022-05-25 15:39:14 -0300

    feat(app) Add tickets  module and controller

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,23 @@ import { EnterprisesModule } from '../enterprises/enterprises.module';
 import { EnterprisesController } from '../enterprises/enterprises.controller';
 import { LocationsModule } from '../locations/locations.module';
 import { LocationsController } from '../locations/locations.controller';
+import { TicketsModule } from '../tickets/tickets.module';
+import { TicketsController } from 'src/tickets/tickets.controller';
 
 @Module({
-  imports: [AuthModule, EnterprisesModule, LocationsModule, UsersModule],
-  controllers: [AppController, EnterprisesController, LocationsController],
+  imports: [
+    AuthModule,
+    EnterprisesModule,
+    LocationsModule,
+    TicketsModule,
+    UsersModule,
+  ],
+  controllers: [
+    AppController,
+    EnterprisesController,
+    LocationsController,
+    TicketsController,
+  ],
   providers: [],
 })
 export class AppModule {}
