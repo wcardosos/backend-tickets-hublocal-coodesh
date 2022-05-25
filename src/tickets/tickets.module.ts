63fbffc1c1e151cde091318a5991f0f4a4f2b5a0
Wagner Cardoso <wagnerdev01@gmail.com>
2022-05-25 15:40:31 -0300

    feat(tickets) Add module dependencies

--- src/tickets/tickets.module.ts
+++ src/tickets/tickets.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TicketsService } from './tickets.service';
 import { TicketsController } from './tickets.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [TicketsController],
-  providers: [TicketsService]
+  providers: [TicketsService, JwtService, PrismaService],
+  exports: [TicketsService],
 })
 export class TicketsModule {}
