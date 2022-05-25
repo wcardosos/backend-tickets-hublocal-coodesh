c1e59f5548d06e6c1fa73f8443067b4997b72d88
Wagner Cardoso <wagnerdev01@gmail.com>
2022-05-24 21:26:16 -0300

    test(e2e/app) Fix imports

--- src/locations/locations.module.ts
+++ src/locations/locations.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { LocationsService } from './locations.service';
 import { LocationsController } from './locations.controller';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [LocationsController],
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -16,7 +16,7 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { HashManager } from '../lib/HashManager';
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
